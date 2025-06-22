96efd42452a1c844a0842b074e658a1453c0f02a
Caleb Thomas <scalny2211@ueab.ac.ke>
2025-06-21 21:41:28 -0700

    code commit

--- musician_connect/settings.py
+++ musician_connect/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'musician_connect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'musician-connect',
         'USER': 'postgres',
         'PASSWORD': 'bakari99',
