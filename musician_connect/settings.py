5947dfb5cf311627d3643a2b3963ec3d3fe49ee7
Caleb Thomas <scalny2211@ueab.ac.ke>
2025-06-21 21:29:32 -0700

    code commit

--- musician_connect/settings.py
+++ musician_connect/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jy&2j41ggs=-f$v7yv7#x1s5rcgq&+o0hrnn0(oop1wy5ahbx7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
